83e4fe1e7f5b77802010c90f04d71afd61f3718c
ChadwickBroski <163284973+ChadwickBroski@users.noreply.github.com>
2026-01-06 20:42:26 +0530

    Delete tictactoe directory

--- tictactoe/tictactoe/__init__.py
+++
@@ -1,4 +0,0 @@
-from .game import TicTacToe
-from .ai import random_ai
-
-__all__ = ["TicTacToe", "random_ai"]
--- tictactoe/tictactoe/ai.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def random_ai(game):
-    moves = game.available_moves()
-    return random.choice(moves) if moves else None
